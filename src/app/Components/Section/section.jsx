7d5ab0b749a97f56ee4f1d56371fcfd924dc4fbe
Kazaryannn <147075312+Kazaryannn@users.noreply.github.com>
2024-06-16 19:04:56 +0400

    k

--- src/app/Components/Section/section.jsx
+++ src/app/Components/Section/section.jsx
@@ -4,7 +4,7 @@ import styles from './section.module.css'
 const Section = () => {
   return (
     <section className={styles.section}>
-     <video autoPlay loop muted playsinline  className={styles.video}>
+     <video autoPlay muted playsinline className={styles.video}>
         <source type="video/mp4" src={'video/video.MP4'}/>
      </video>
     </section>
