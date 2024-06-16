c5a4c49a18dd9b351bc749f316ab3767afef9131
Kazaryannn <147075312+Kazaryannn@users.noreply.github.com>
2024-06-16 19:20:35 +0400

    k

--- src/app/Components/Section/section.jsx
+++ src/app/Components/Section/section.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
 import styles from './section.module.css'
 
+
+
 const Section = () => {
-   
-        if(typeof window !== 'undefined' ){
-            window.onload = function (){
-            document.getElementById("autoplay").play
-            }
+    if(typeof window !== 'undefined' ){
+        window.onload = function (){
+        document.getElementById("autoplay").play
         }
-    
+    }
   return (
     <section className={styles.section}>
-     <video autoPlay muted playsinline className={styles.video} id='auyoplay'>
+     <video autoPlay muted className={styles.video} id='autoplay'>
         <source type="video/mp4" src={'video/video.MP4'}/>
      </video>
     </section>
